a409d2747562e49182aba3082b363d04b3b71b73
Diannalp1 <>
2021-11-12 14:35:17 +0000

    mmc

--- src/exercise5/AscendingSort.java
+++ src/exercise5/AscendingSort.java
@@ -8,18 +8,30 @@ public class AscendingSort {
         Scanner input = new Scanner(System.in);
 
         System.out.print("Enter number count: ");
-        int numberCount = input.nextInt();
+        int size = input.nextInt();
+        int arr[] = new int[size];
+        int temp;
 
-        int[] numbers = new int[numberCount];
+        //int[] numbers = new int[size];
 
         System.out.println("Enter numbers: ");
-        for (int index = 0; index < numbers.length; index++) {
-            numbers[index] = input.nextInt();
+        for (int index = 0; index < size; index++) {
+            arr[index] = input.nextInt();
+        }
+
+        for (int index = 0; index < size; index++) {
+          for (int j = 0; j < size-1; j++) {
+            if (arr[j] > arr[j+1]) {
+              temp = arr[j];
+              arr[j] = arr[j+1];
+              arr[j+1] = temp;
+            }
+          }
         }
 
         // Sort numbers in ascending order
 
-        String numbersAsString = Arrays.toString(numbers);
-        System.out.println("Sorted numbers: " + numbersAsString);
+        String arrsAsString = Arrays.toString(arr);
+        System.out.println("Sorted numbers: " + arrsAsString);
     }
 }
